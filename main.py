c0d96b55eb508cacce3ecf6c549080a399bcf304
Bagri <bagri.apsk@gmail.com>
2023-10-10 15:28:31 +0530

    Market Data changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fyers_apiv3.FyersWebsocket import data_ws
 
+
 def onmessage(message):
     """
     Callback function to handle incoming messages from the FyersDataSocket WebSocket.
@@ -37,23 +38,32 @@ def onopen():
     """
     # Specify the data type and symbols you want to subscribe to
     data_type = "SymbolUpdate"
+    # data_type = "DepthUpdate"
+
+
     # Subscribe to the specified symbols and data type
-    symbols = ['NSE:SBIN-EQ']
-    access_token = "asdasdasdasdadaskdjflsdkjfdslkfjsdlkfjoritroiturotu"
-    fyers = data_ws.FyersDataSocket(
-        access_token=access_token,  # Access token in the format "appid:accesstoken"
-        log_path="",  # Path to save logs. Leave empty to auto-create logs in the current directory.
-        litemode=True,  # Lite mode disabled. Set to True if you want a lite response.
-        write_to_file=False,  # Save response in a log file instead of printing it.
-        reconnect=True,  # Enable auto-reconnection to WebSocket on disconnection.
-        on_connect=onopen,  # Callback function to subscribe to data upon connection.
-        on_close=onclose,  # Callback function to handle WebSocket connection close events.
-        on_error=onerror,  # Callback function to handle WebSocket errors.
-        on_message=onmessage) # Callback function to handle incoming messages from the WebSocket.
+    symbols = ['NSE:SBIN-EQ', 'NSE:ADANIENT-EQ']
     fyers.subscribe(symbols=symbols, data_type=data_type, channel=15)
 
     # Keep the socket running to receive real-time data
-  #  fyers.keep_running()
-    fyers.connect()
+    fyers.keep_running()
 
-onopen()
+
+# Replace the sample access token with your actual access token obtained from Fyers
+access_token = "0NTA5OCwibmJmIjoxNjk2OTE0NDk4LCJhdWQiOiJbXCJkOjFcIl0iLCJzdWIiOiJhdXRoX2NvZGUiLCJkaXNwbGF5X25hbWUiOiJYUzIxODA0Iiwib21zIjoiSzEiLCJoc21fa2V5IjoiOWM1NjhlN2FlNjQ0MzdkMjk3MDgyZWUwZGVkY2MxMmI3ZWM5NzlkNTljMjVhZTdlZTFiODIyN2IiLCJub25jZSI6IiIsImFwcF9pZCI6IkVPU0JJTVg3MFciLCJ1dWlkIjoiZTA3ZjM5MWQwYzA0NDQ0Yzk5ODhmZjRmYTVmN2EyYTEiLCJpcEFkZHIiOiIwLjAuMC4wIiwic2NvcGUiOiIifQ.MpkKv0_E4K-Ku23maQz6b6Hb91EWY6vHvFcvXcWS17A"
+
+# Create a FyersDataSocket instance with the provided parameters
+fyers = data_ws.FyersDataSocket(
+    access_token=access_token,       # Access token in the format "appid:accesstoken"
+    log_path="",                     # Path to save logs. Leave empty to auto-create logs in the current directory.
+    litemode=False,                  # Lite mode disabled. Set to True if you want a lite response.
+    write_to_file=False,              # Save response in a log file instead of printing it.
+    reconnect=True,                  # Enable auto-reconnection to WebSocket on disconnection.
+    on_connect=onopen,               # Callback function to subscribe to data upon connection.
+    on_close=onclose,                # Callback function to handle WebSocket connection close events.
+    on_error=onerror,                # Callback function to handle WebSocket errors.
+    on_message=onmessage             # Callback function to handle incoming messages from the WebSocket.
+)
+
+# Establish a connection to the Fyers WebSocket
+fyers.connect()
